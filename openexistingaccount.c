f61662fb281f17ff616d4ec2729875c7682c9aba
Asmaasa3d <48691633+Asmaasa3d@users.noreply.github.com>
2022-05-09 23:21:56 +0200

    Update openexistingaccount.c

--- openexistingaccount.c
+++ openexistingaccount.c
@@ -43,74 +43,103 @@ typedef struct List
 void create_new_account();
 void Open_Existing_Account();
 void Make_Transaction(clientaccount *clnt, List *pl);
-void Change_Account_Status(clientaccount *clnt, List *pl);
-void Get_Cash(clientaccount *clnt);
-
-void Deposit_in_Account(clientaccount *clnt);
-
+clientaccount *Change_Account_Status(clientaccount *clnt, List *pl);
+clientaccount *Get_Cash(clientaccount *clnt, List *pl);
+clientaccount *Deposit_in_Account(clientaccount *clnt);
 void admin_main_menu(List *pl);
 void Return_to_admin_main_menu(List *pl);
-
 void List_voidInit(List *pl);
 void List_voidInsertList(int pos, List *pl, ClientNode *val);
-void List_voidRetriveList(int pos, List *pl, clientaccount *clnt);
+clientaccount *List_voidRetriveList(int pos, List *pl, clientaccount *clnt);
 clientaccount *get_pointer_to_clint_by_ID(int id);
-
+void print_client_acc(clientaccount *c);
 void create_new_account();
 
-int main()
+int main(char *argv[])
 {
 
-  printf("hello asmaa");
-
   List *pl;
-  printf("hello asmaa");
+  pl = malloc(sizeof(ClientNode) * 2);
+
   List_voidInit(pl);
-  printf("hello alaa");
 
   clientaccount *asma;
-  strcpy(asma->full_address,"ASmaa saaed sadd" );
-  asma->age=20;
-  asma->balance=9000;
-  asma->National_id=90;
-  strcpy(asma->status,"Active" );
+  asma = malloc(sizeof(clientaccount));
+  strcpy(asma->full_address, "ASmaa saaed sadd");
+  asma->age = 20;
+  asma->balance = 9000;
+  asma->National_id = 90;
+  strcpy(asma->status, "Active");
 
   ClientNode *newn;
-  newn->data=asma;
-  newn->Next=NULL;
-List_voidInsertList(0,pl,newn);
-  
+  newn = malloc(sizeof(ClientNode));
 
-  // admin_main_menu(pl);
+  newn->data = asma;
+  newn->Next = NULL;
+  List_voidInsertList(0, pl, newn);
+
+  clientaccount *fatma;
+  fatma = malloc(sizeof(clientaccount));
+  strcpy(fatma->full_address, "alaa wi fatima");
+  fatma->age = 20;
+  fatma->balance = 12000;
+  fatma->National_id = 80;
+  strcpy(fatma->status, "Active");
+
+  ClientNode *newf;
+  newf = malloc(sizeof(ClientNode));
+
+  newf->data = fatma;
+  newf->Next = NULL;
+  List_voidInsertList(0, pl, newf);
+
+  admin_main_menu(pl);
   return 0;
 }
+
+/**********************************************/
+/**********************************************/
+clientaccount *clnt;
 
 void Open_Existing_Account(int rgst, List *pl)
 {
 
   int feat_no;
-  clientaccount clnt;
-  if (rgst == 0)
-  {
-    printf("Welcome! pls enter your Bank Account ID:..");
-    scanf("%d", &clnt_id);
-    List_voidRetriveList(clnt_id, pl, &clnt);
-  }
+
+  // if (rgst == 0)
+  // {
+  printf("Welcome! pls enter your Bank Account ID:..\n");
+  scanf("%d", &clnt_id);
+  // printf("clinnnn %d",clnt_id);
+  clnt = List_voidRetriveList(clnt_id, pl, clnt);
+  // }
   //  should verify this id
+  printf("\n1- Make Transaction\n ");
+
+  printf("2- Change Account Status\n ");
+  printf("3- Get Cash\n ");
+
+  printf("4- Deposit in Account\n ");
+  printf("5- Return to main menu\n ");
+
   printf("Thank you -_-, pls choose the feature you want! ");
+
   scanf("%d", &feat_no);
+
   switch (feat_no)
   {
   case 1:
-    Make_Transaction(&clnt, pl);
+    Make_Transaction(clnt, pl);
     break;
   case 2:
-    Change_Account_Status(&clnt, pl);
+    clnt = Change_Account_Status(clnt, pl);
     break;
   case 3:
-    Get_Cash(&clnt);
+    clnt = Get_Cash(clnt, pl);
+    break;
+
   case 4:
-    Deposit_in_Account(&clnt);
+    Deposit_in_Account(clnt);
     break;
   case 5:
     Return_to_admin_main_menu(pl);
@@ -119,27 +148,33 @@ void Open_Existing_Account(int rgst, List *pl)
     printf("please enter valid number");
     break;
   }
+  Open_Existing_Account(1, pl);
 }
 
+/**********************************************/
+/**********************************************/
+
+clientaccount *rcv;
 void Make_Transaction(clientaccount *clnt, List *pl)
 {
+
   int to_id;
   int mny_amnt;
   printf("Welcome! pls enter the Bank Account ID you want to transfer money to:..");
   scanf("%d", &to_id);
 
-  clientaccount *rcv;
-  List_voidRetriveList(to_id, pl, rcv);
+  rcv = List_voidRetriveList(to_id, pl, rcv);
 
   //  should verify this id
 
   printf("Thank you -_-, pls enter amount of money to transfer it to the this account ! ");
   scanf("%d", &mny_amnt);
 
+  printf("%s", rcv->status);
   // You have to make sure that both accounts are Active.
-  if (clnt->status == "Active" && rcv->status == "Active")
+  if (strcmp(clnt->status, "Active") == 0 & strcmp(rcv->status, "Active") == 0)
   {
-    if (clnt->balance < mny_amnt)
+    if (clnt->balance > mny_amnt)
     {
       clnt->balance -= mny_amnt;
       rcv->balance += mny_amnt;
@@ -154,15 +189,15 @@ void Make_Transaction(clientaccount *clnt, List *pl)
   {
     printf("the accounts are not active please activate them to complete this action:(");
   }
-  getchar();
-  Open_Existing_Account(1, pl);
 }
 
+/**********************************************/
+/**********************************************/
 
-void Change_Account_Status(clientaccount *clnt, List *pl)
+clientaccount *Change_Account_Status(clientaccount *clnt, List *pl)
 {
-  printf("PLS choose the number of the Bank Account Status, you want for this account");
-  printf("1- Set Account to Active State\nSet Account to Restricted State\n 3- Set Account to Closed\n");
+  printf("PLS choose the number of the Bank Account Status, you want for this account\n");
+  printf("1- Set Account to Active State\n 2- Set Account to Restricted State\n 3- Set Account to Closed\n");
   int st;
   scanf("%d", &st);
   switch (st)
@@ -181,138 +216,164 @@ void Change_Account_Status(clientaccount *clnt, List *pl)
     printf("please enter valid status");
     break;
   }
-  getchar();
-  Open_Existing_Account(1, pl);
+  return clnt;
 }
 
-void Get_Cash(clientaccount *clnt)
+/**********************************************/
+/**********************************************/
+
+clientaccount *Get_Cash(clientaccount *clnt, List *pl)
 {
   int mny_amnt;
 
-  printf("pls enter amount of money you want from this account");
+  printf("pls enter amount of money you want from this account\n");
   scanf("%d", &mny_amnt);
-  if (clnt->balance < mny_amnt)
+  printf("Your current balance is %d", clnt->balance);
+
+  if (clnt->balance > mny_amnt)
   {
     clnt->balance -= mny_amnt;
-    printf("Done!");
+    printf("Done! Your current balance now is %d\n", clnt->balance);
   }
   else
   {
     printf("You don't have enough money, the transaction won’t be completed :(");
   }
+  return clnt;
+  // getchar();
+  // Open_Existing_Account(1, pl);
 }
 
-void Deposit_in_Account(clientaccount *clnt)
+/**********************************************/
+/**********************************************/
+
+clientaccount *Deposit_in_Account(clientaccount *clnt)
 {
 
   int mny_amnt;
+  printf("Your current balance is %d", clnt->balance);
 
   printf("pls enter amount of money you want to add to this account.");
+
   scanf("%d", &mny_amnt);
   clnt->balance += mny_amnt;
-  printf("Done!");
+  printf("Done! Your current balance now is %d\n", clnt->balance);
+
+  return clnt;
 }
 
+/**********************************************/
+/**********************************************/
 void admin_main_menu(List *pl)
 {
   int current_feature = 0;
-  printf("\t\t\tSystem Main Menu\t\t\t");
-  printf("1- Create New Account");
-  printf("2- Open Existing Account");
-  printf("3- Exit the System");
+  printf("\t\t**************System Main Menu*****************\t\t\n");
+  printf("1- Create New Account\n");
+  printf("2- Open Existing Account\n");
+  printf("3- Exit the System\n");
   printf("Please choose the feature you need!:... ");
   scanf("%d", &current_feature);
-  if (current_feature == 1)
-  {
-    create_new_account(pl);
-  }
-  else if (current_feature == 2)
+  // if (current_feature == 1)
+  // {
+  //   create_new_account(pl);
+  // }
+  if (current_feature == 2)
   {
     Open_Existing_Account(0, pl);
   }
 }
+
+/**********************************************/
+/**********************************************/
 
 void Return_to_admin_main_menu(List *pl)
 {
   admin_main_menu(pl);
 }
 
-clientaccount *get_pointer_to_clint_by_ID(int id)
-{
-}
+/**********************************************/
+/**********************************************/
 
 void List_voidInit(List *pl)
 {
+  pl->head = malloc(sizeof(ClientNode));
   pl->head = NULL;
   pl->Size = 0;
 }
-// fatama task1 create new account
+// // fatama task1 create new account
 
 void List_voidInsertList(int pos, List *pl, ClientNode *val)
 {
 
   ClientNode *pn;
   pn = val;
-  // pn->Next = NULL;
+  // // pn->Next = NULL;
   if (pos == 0)
   {
-    ClientNode *tmp = pl->head;
+
+    
+    ClientNode *tmp;
+    tmp = pl->head;
     pl->head = pn;
     pl->head->Next = tmp;
   }
-  else
-  {
-    ClientNode *tmp = pl->head;
-    for (int i = 0; i < pos - 1; i++)
-    {
-      tmp = tmp->Next;
-    }
-    pn->Next = tmp->Next;
-    tmp->Next = pn;
-  }
+
   (pl->Size)++;
 }
 
-void List_voidRetriveList(int pos, List *pl, clientaccount *clnt)
+clientaccount *List_voidRetriveList(int pos, List *pl, clientaccount *clnt)
 {
-  ClientNode *tmp = pl->head;
-  // for (int i = 0; i < pos; i++)
+
+  ClientNode *tmp = malloc(sizeof(ClientNode));
+
+  tmp = pl->head;
+
   while (tmp->data->National_id != pos)
   {
     tmp = tmp->Next;
   }
   clnt = tmp->data;
+  return clnt;
 }
-
-void create_new_account(List *pl)
+void print_client_acc(clientaccount *c)
 {
-
-  clientaccount *newclientacc;
-  printf("Pls enter your fullName:   ");
-  scanf("%c", newclientacc->full_name);
-  printf("Pls enter your address:   ");
-  scanf("%c", newclientacc->full_address);
-
-  printf("Pls enter your nationID:   ");
-  scanf("%d", &(newclientacc->National_id));
-
-  printf("Pls enter your Age:   ");
-  scanf("%d", &(newclientacc->age));
-
-  if (newclientacc->age < 21)
-  {
-    printf("Pls enter your Guardian:   ");
-    scanf("%c", newclientacc->guardian_National_id);
-  }
-  printf("Pls enter your Status:   ");
-
-  scanf("%c", newclientacc->status);
-
-  newclientacc->balance = 0;
-  newclientacc->account_passowrd = rand();
-
-  ClientNode *newnode;
-  newnode->data = newclientacc;
-  newnode->Next = NULL;
-  List_voidInsertList(0, pl, newnode);
+  printf("%d", c->balance);
+  printf("%s", c->full_name);
+  printf("%d", c->National_id);
 }
+
+/**********************************************/
+/**********************************************/
+
+// void create_new_account(List *pl)
+// {
+
+//   clientaccount *newclientacc;
+//   printf("Pls enter your fullName:   ");
+//   scanf("%c", newclientacc->full_name);
+//   printf("Pls enter your address:   ");
+//   scanf("%c", newclientacc->full_address);
+
+//   printf("Pls enter your nationID:   ");
+//   scanf("%d", &(newclientacc->National_id));
+
+//   printf("Pls enter your Age:   ");
+//   scanf("%d", &(newclientacc->age));
+
+//   if (newclientacc->age < 21)
+//   {
+//     printf("Pls enter your Guardian:   ");
+//     scanf("%c", newclientacc->guardian_National_id);
+//   }
+//   printf("Pls enter your Status:   ");
+
+//   scanf("%c", newclientacc->status);
+
+//   newclientacc->balance = 0;
+//   newclientacc->account_passowrd = rand();
+
+//   ClientNode *newnode;
+//   newnode->data = newclientacc;
+//   newnode->Next = NULL;
+//   List_voidInsertList(0, pl, newnode);
+// }
