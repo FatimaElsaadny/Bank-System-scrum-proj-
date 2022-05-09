441cb0c159bc37a5b4c89d8478ab0a50bdd2c319
FatimaElsaadny <Tima.elsaadny.342y@gmail.com>
2022-05-09 02:10:23 +0200

    Create Admin class and some basic code for create account

---
+++ Admin.c
@@ -0,0 +1,44 @@
+// ***************************************************** //
+//*************** Import Libraries ********************* //
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// ***************************************************** //
+//*************** Functions declaration **************** //
+
+void create_new_account();
+
+// ***************************************************** //
+//*************** Struct declaration **************** //
+typedef struct Person
+{
+    char full_name[150];
+    char full_address[150];
+    char National_id[15];
+    char guardian_National_id[15];
+    int age;
+    int account_
+
+} person;
+
+// ***************************************************** //
+//*************** Main Method ********************* //
+int main()
+{
+
+    for (int i = 0; i < 5; i++)
+        printf(" %d ", rand());
+
+    return 0;
+}
+
+// ***************************************************** //
+//*************** Functions definition ********************* //
+
+void create_new_account()
+{
+    person p;
+}
+
+// ******************************
